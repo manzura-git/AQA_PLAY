e34a4320cd2d90658c44b61768edc37b3e403c99
manzura-git <manzura.qayumova@alif.tj>
2026-04-08 14:15:23 +0500

    add task 26 02

--- the_internet_tester.py
+++ the_internet_tester.py
@@ -3,6 +3,12 @@ from playwright.sync_api import sync_playwright
 
 BASE_URL = "https://the-internet.herokuapp.com/"
 TITLE = "the-internet"
+TITLE_FORM_AUTH = "Form Authentication"
+
+def navigate_to_example(example_name: str):
+    el_form_auth = page.get_by_role("link", name=example_name)
+    el_form_auth.click()
+    return page.url
 
 
 with sync_playwright() as drv:
@@ -16,6 +22,11 @@ with sync_playwright() as drv:
     el_head_text = el_head.inner_text()
     assert TITLE in el_head_text, \
         f"Заголовок '{el_head_text}' не содержит '{TITLE}'"
+
+    # 🌐26x02: Навигация по ссылкам
+    current_url = navigate_to_example(TITLE_FORM_AUTH)
+    assert "/login" in current_url, "Не та страница!"
+    print(f"✅ Перешли в: Form Authentication | URL: {current_url}")
 
 
 
